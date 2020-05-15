cf2ea05fe14fe7918b24c25fdae27878fa49dace
Aleksey-Grebenkin <alexgreben24@gmail.com>
2020-05-15 17:11:02 +0700

    create

---
+++ laba 5.2/task.hpp
@@ -0,0 +1,14 @@
+//
+//  task.hpp
+//  laba 5.2
+//
+//  Created by user on 06/05/2020.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+#ifndef task_hpp
+#define task_hpp
+
+#include <stdio.h>
+
+#endif /* task_hpp */
